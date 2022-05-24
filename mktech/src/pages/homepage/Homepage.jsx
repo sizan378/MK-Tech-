a220c86143ce8717b7229737f509e5b291b52cca
sizan378 <sizanmahmud08@gmail.com>
2022-05-24 16:16:09 +0600

    first commit

---
+++ mktech/src/pages/homepage/Homepage.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+// import { useLocation } from "react-router";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./homepage.css";
+import axios from "axios"
+import { useLocation } from 'react-router-dom';
+
+export default function Homepage() {
+  const [posts,setPosts] = useState([]);
+  const {search} = useLocation();
+
+
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      const res = await axios.get("/posts"+search)
+      setPosts(res.data)
+    }
+    fetchPosts()
+  },[search])
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
---
+++ mktech/src/pages/user/User.jsx
@@ -0,0 +1,11 @@
+// import { useContext, useState } from "react";
+import "./user.css";
+
+export default function User() {
+  
+  return (
+    <div className="write">
+      
+    </div>
+  );
+}
